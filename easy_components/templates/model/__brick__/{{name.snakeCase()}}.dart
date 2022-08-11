e9b9294694c3df3c95112ea6dc27bd8156045612
Lennard Deurman <lennarddeurman@live.nl>
2022-08-11 11:01:08 +0200

    Changed the class_props macro to support lists

--- easy_components/templates/model/__brick__/{{name.snakeCase()}}.dart
+++ easy_components/templates/model/__brick__/{{name.snakeCase()}}.dart
@@ -1,10 +1,11 @@
 {{#use_equatable}}import 'package:equatable/equatable.dart';{{/use_equatable}}{{#use_copy_with}}
+import 'package:copy_with_extension/copy_with_extension.dart';
 
 part '{{name.snakeCase()}}.g.dart';
 
 @CopyWith({{#generate_copy_with_null}}generateCopyWithNull:true{{/generate_copy_with_null}}){{/use_copy_with}}
 class {{name.pascalCase()}} {{#use_equatable}}extends Equatable{{/use_equatable}} {
-  {{class_props}}
+  {{{class_props}}}
 
   const {{name.pascalCase()}}({{constructor}});
 
